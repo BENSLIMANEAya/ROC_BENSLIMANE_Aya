a3e42391a5f9b78b7b9e330c084d48fd52b393e0
BENSLIMANEAya <118344561+BENSLIMANEAya@users.noreply.github.com>
2023-03-20 09:44:36 +0100

    Update ToolBox.c

--- Embedded/Robot_Labre_Kinigbe_Sog.X/ToolBox.c
+++ Embedded/Robot_Labre_Kinigbe_Sog.X/ToolBox.c
@@ -1,10 +1,13 @@
 #include "Toolbox.h"
+#include "main.h"
+#define PI  3.141592653589793
 float Abs(float value)
 {
 if (value >= 0)
 return value;
 else return -value;
 }
+
 float Max(float value, float value2)
 {
 if (value > value2)
@@ -12,6 +15,7 @@ return value;
 else
 return value2;
 }
+
 float Min(float value, float value2)
 {
 if (value < value2)
@@ -19,20 +23,23 @@ return value;
 else
 return value2;
 }
+
 float LimitToInterval(float value, float lowLimit, float highLimit)
 {
 if (value > highLimit)
 value = highLimit;
 else if (value < lowLimit)
 value = lowLimit;
+
 return value;
 }
+
 float RadianToDegree(float value)
 {
 return value / PI * 180.0;
 }
+
 float DegreeToRadian(float value)
 {
 return value * PI / 180.0;
 }
-
