9ed6abcd3eb7db29a05daed9be44336c05431a75
BENSLIMANEAya <118344561+BENSLIMANEAya@users.noreply.github.com>
2023-03-20 09:25:52 +0100

    Update timer.h

--- Embedded/Robot_Benslimane.X/timer.h
+++ Embedded/Robot_Benslimane.X/timer.h
@@ -1,12 +1,9 @@
-/* 
- * File:   timer.h
- * Author: Table2
- *
- * Created on 16 novembre 2022, 11:11
- */
-
 #ifndef TIMER_H
 #define TIMER_H
+
+extern unsigned long timestamp;
+
+
 void InitTimer23(void);
 void InitTimer1(void);
 #endif 
