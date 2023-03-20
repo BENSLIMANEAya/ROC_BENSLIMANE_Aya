c590c798a84caee5c0a8824c8ddcde0fc02039f5
BENSLIMANEAya <118344561+BENSLIMANEAya@users.noreply.github.com>
2023-03-20 09:38:25 +0100

    Update ADC.h

--- Embedded/Robot_Labre_Kinigbe_Sog.X/ADC.h
+++ Embedded/Robot_Labre_Kinigbe_Sog.X/ADC.h
@@ -1,5 +1,7 @@
 #ifndef ADC_H
-#define ADC_H
+#define	ADC_H
+
+
 
 void InitADC1(void);
 void __attribute__((interrupt, no_auto_psv)) _AD1Interrupt(void);
@@ -8,4 +10,8 @@ unsigned int * ADCGetResult(void);
 unsigned char ADCIsConversionFinished(void);
 void ADCClearConversionFinishedFlag(void);
 
-#endif /* ADC_H */
+
+
+
+
+#endif	/* ADC_H */
