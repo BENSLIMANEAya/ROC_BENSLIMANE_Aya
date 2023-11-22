6dec88b510a6c9bf27e6ed6b91640df2b55e84d9
TABLE3 <touloniut@gmail.com>
2023-11-22 12:36:41 +0100

    TP1

---
+++ simple_peripheral_CC26X2R1_LAUNCHXL_tirtos_ccs/TacheADC/TacheADC.h
@@ -0,0 +1,17 @@
+/*
+ * TacheADC.h
+ *
+ *  Created on: 22 nov. 2023
+ *      Author: TP-EO-5
+ */
+
+#ifndef TACHEADC_TACHEADC_H_
+#define TACHEADC_TACHEADC_H_
+
+void TacheADC_CreateTask(void);
+void turn_on_LEDS(void);
+void turn_off_LEDS(void);
+void TacheADC_CreateTask(void);
+void TacheADC_Create_Task(void);
+void TacheADC_init(void);
+#endif /* TACHEADC_TACHEADC_H_ */
