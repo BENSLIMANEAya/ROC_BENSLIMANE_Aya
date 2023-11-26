354c2ea04c4d02149d42551f86e737643403d98f
BENSLIMANEAya <118344561+BENSLIMANEAya@users.noreply.github.com>
2023-11-26 10:52:07 +0100

    Update TacheADC.h

--- simple_peripheral_CC26X2R1_LAUNCHXL_tirtos_ccs/TacheADC/TacheADC.h
+++ simple_peripheral_CC26X2R1_LAUNCHXL_tirtos_ccs/TacheADC/TacheADC.h
@@ -5,13 +5,14 @@
  *      Author: TP-EO-5
  */
 
+
 #ifndef TACHEADC_TACHEADC_H_
 #define TACHEADC_TACHEADC_H_
 
 void TacheADC_CreateTask(void);
 void turn_on_LEDS(void);
 void turn_off_LEDS(void);
-void TacheADC_CreateTask(void);
-void TacheADC_Create_Task(void);
+void myClockSwiFxn(uintptr_t arg0);
+void Sampling(uint_least8_t Board_ADC_Number);
 void TacheADC_init(void);
 #endif /* TACHEADC_TACHEADC_H_ */
